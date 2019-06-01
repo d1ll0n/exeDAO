8397535f48d9af462b46e860e8b937b480e6eee1
Matt Czernik <mattczernik@Matts-MacBook-Air.local>
2019-06-01 16:27:10 -0400

    add dependencies bitch

--- app-frontend/src/index.js
+++ app-frontend/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import store, { history } from './store';
+import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const target = document.querySelector('#root');
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target,
+);
