cf177bf470fc6b4f06d9789e8462ad3daa6f6dbc
Matt Czernik <mattczernik@Matts-MacBook-Air.local>
2019-06-01 16:35:40 -0400

    setup storybook

--- app-frontend/.storybook/config.js
+++ app-frontend/.storybook/config.js
@@ -1,9 +1,21 @@
 import { configure } from '@storybook/react';
 
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { configure, addDecorator } from '@storybook/react';
+
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+  req.keys().forEach((filename) => req(filename));
 }
+
+const withProviders = (story) => {
+  <MuiThemeProvider theme={theme}>{story()}</MuiThemeProvider>;
+};
+
+addDecorator(withKnobs({ escapeHTML: false }));
+addDecorator(withProviders);
 
 configure(loadStories, module);
