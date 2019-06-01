095da02cde6dd87d5ae6fd2760b45472df006d0a
d1ll0n <dillonkellar@protonmail.com>
2019-06-01 09:38:16 -0500

    cool shit

--- sol/contracts/ExeLib.sol
+++ sol/contracts/ExeLib.sol
@@ -1,24 +1,29 @@
 pragma solidity ^0.5.6;
 
 library ExeLib {
+  struct Function {
+    address functionAddress;
+    bool call;
+  }
+
   function isPermissible (bytes memory bytecode, bool disallowDestruct)
   internal pure returns (bool permissible) {
     uint size = bytecode.length;
+    permissible = true;
     assembly {
       let ptr := add(bytecode, 0x20)
       for { let i := 0 } lt(i, size) { i := add(i, 0x1) } {
         let op := shr(0xf8, mload(add(ptr, i)))
         switch op
-        case 0xf2 { break } // callcode
-        case 0xf4 { break } // delegatecall
-        case 0x55 { break } // sstore
-        case 0xff { if disallowDestruct { break } } // selfdestruct
+        case 0xf2 { permissible := 0 break } // callcode
+        case 0xf4 { permissible := 0 break } // delegatecall
+        case 0x55 { permissible := 0 break } // sstore
+        case 0xff { if disallowDestruct { permissible := 0 break } } // selfdestruct
         default {
           let isPush := and(lt(op, 0x80), gt(op, 0x5f))
           if eq(isPush, 0x1) { i := add(i, sub(0x21, sub(0x80, op))) }
         }
       }
-      permissible := 1
     }
   }
 
@@ -27,17 +32,31 @@ library ExeLib {
     assembly {
       let start := add(bytecode, 0x20)
       let delegateTo := create(0, start, size)
-      let freeptr := mload(0x40)
-      let delegateSuccess := delegatecall(gas, delegateTo, 0, 0, freeptr, 0)
+      let retptr := mload(0x40)
+      let delegateSuccess := delegatecall(gas, delegateTo, 0, 0, retptr, 0)
+      let retsize := returndatasize
+      returndatacopy(retptr, 0, retsize)
+      let freeptr := add(retptr, retsize)
       mstore(freeptr, 0x41c0e1b500000000000000000000000000000000000000000000000000000000)
       let selfdestructSuccess := call(gas, delegateTo, 0, freeptr, 0x20, 0, 0)
-      if eq(and(delegateSuccess, selfdestructSuccess), 0) {
-        revert(0, 0)
-      }
+      let success := and(delegateSuccess, selfdestructSuccess)
+      if success { return(retptr, retsize) }
+      revert(0, 0)
+    }
+  }
+
+  function delegateExecute(address delegateTo) internal {
+    assembly {
+      let startCalldata := mload(0x40)
+      calldatacopy(startCalldata, 0, calldatasize)
+      let retptr := add(startCalldata, calldatasize)
+      let delegateSuccess := delegatecall(gas, delegateTo, startCalldata, calldatasize, retptr, 0)
+      if delegateSuccess { return (retptr, returndatasize) }
+      revert(0, 0)
     }
   }
   
-  function byteCodeAt(address deployedAddress)
+  function bytecodeAt(address deployedAddress)
   internal view returns (bytes memory bytecode) {
     uint size;
     assembly { size := extcodesize(deployedAddress) }
--- sol/contracts/exeDAO.sol
+++ sol/contracts/exeDAO.sol
@@ -5,17 +5,18 @@ import "./ExeLib.sol";
 import "./Permissioned.sol";
 
 contract exeDAO is Permissioned {
+  using ExeLib for address;
   using ExeLib for bytes;
-  mapping(bytes4 => address) public functions;
+  mapping(bytes4 => ExeLib.Function) public functions;
 
   constructor(uint64 shares, bytes4[] memory funcSigs, uint8[] memory requirements) public payable {
     require(funcSigs.length == requirements.length);
     for (uint i = 0; i < funcSigs.length; i++) {
       DaoLib.ProposalRequirement req = DaoLib.ProposalRequirement(requirements[i]);
       require(req != DaoLib.ProposalRequirement.Default);
-
+      proposalRequirements[funcSigs[i]] = req;
     }
-
+    _mintShares(msg.sender, shares);
   }
 
   function safeExecute(bytes calldata bytecode) external {
@@ -29,12 +30,38 @@ contract exeDAO is Permissioned {
     bytecode.delegateExecute();
   }
 
+  function addFunctions(
+    address functionAddress, bool[] calldata isCall,
+    bytes4[] calldata funcSigs
+  ) external {
+    require(functionAddress.bytecodeAt().isPermissible(true), "Bytecode not allowed");
+    require(funcSigs.length == isCall.length, "Inconsistent input");
+    if (!voteAndContinue()) return;
+    for (uint i = 0; i < funcSigs.length; i++) {
+      functions[funcSigs[i]] = ExeLib.Function({
+        functionAddress: functionAddress,
+        call: isCall[i]
+      });
+    }
+  }
+
+  function removeFunction(bytes4[] calldata funcSigs) external {
+    for (uint i = 0; i < funcSigs.length; i++) {
+      if (!voteAndContinue()) return;
+      functions[funcSigs[i]].functionAddress = address(0);
+    }
+  }
+
   function mintShares(address recipient, uint64 amount) external {
     if (!voteAndContinue()) return;
     _mintShares(recipient, amount);
   }
 
   function () external payable {
-
+    address functionAddress = functions[msg.sig].functionAddress;
+    if (functionAddress != address(0)) {
+      if (!voteAndContinue()) return;
+      functionAddress.delegateExecute();
+    }
   }
 }
