3087723d78d6195d93a8b743362645152742091a
Matt Czernik <mattczernik@Matts-MacBook-Air.local>
2019-06-01 21:33:26 -0400

    lerna setup

---
+++ packages/exedao-ui/src/AppBar/styles.js
@@ -0,0 +1,28 @@
+const styles = (theme) => ({
+  toolbar: {
+    paddingLeft: 25,
+    paddingRight: 25,
+    minHeight: 70,
+    backgroundColor: theme.palette.palette.white,
+  },
+  logoWrapper: {
+    flexGrow: 1,
+  },
+  logo: {
+    fontSize: 18,
+    letterSpacing: 0.5,
+  },
+  tabsWrapper: {
+    flexGrow: 0,
+  },
+  styledTab: {
+    fontSize: 15.2,
+    minWidth: 120,
+  },
+  labelContainer: {
+    paddingRight: 0,
+    paddingLeft: 0,
+  },
+});
+
+export default styles;
