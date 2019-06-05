6646230ed032c033ede55711045eeb06d0273320
Matt Czernik <matt.czernik@gmail.com>
2019-06-05 17:03:38 -0400

    forgot box shadows

--- packages/exedao-ui/src/EntryPageCard/styles.js
+++ packages/exedao-ui/src/EntryPageCard/styles.js
@@ -2,6 +2,7 @@ const styles = (theme) => ({
   card: {
     width: 275,
     height: 125,
+    boxShadow: ' 0px 1px 3px 0px #D4D4D5, 0px 0px 0px 1px #D4D4D5',
   },
   contentWrapper: {
     height: '100%',
