07519f3141b878449ff1905f3e30bc294abf1d75
Kinrezc <matt.czernik@gmail.com>
2019-06-22 14:33:18 -0400

    add light grey 2 theme

--- packages/exedao-ui/src/ProposalDetailPage/styles.js
+++ packages/exedao-ui/src/ProposalDetailPage/styles.js
@@ -1,18 +1,18 @@
-const styles = (theme) => ({
-    votesBar: {
-        borderRadius: 20,
-        backgroundColor: theme.palette.palette.green,
-      },
-      rootVotesBar: {
-        borderRadius: 20,
-        backgroundColor: theme.palette.palette.red,
-      },
-      votesLabel: {
-        marginTop: 7,
-        fontSize: 9,
-        color: '#707070',
-        letterSpacing: 0.05,
-      },
+const styles = theme => ({
+  votesBar: {
+    borderRadius: 20,
+    backgroundColor: theme.palette.palette.green
+  },
+  rootVotesBar: {
+    borderRadius: 20,
+    backgroundColor: theme.palette.palette.red
+  },
+  votesLabel: {
+    marginTop: 7,
+    fontSize: 9,
+    color: theme.palette.palette.veryLightGrey,
+    letterSpacing: 0.05
+  }
 });
 
 export default styles;
