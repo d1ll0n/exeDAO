b636b1bad886aa9d20d01ece63f79f26f3478ae1
d1ll0n <dillonkellar@protonmail.com>
2019-06-26 14:22:28 -0500

    updated exedao middleware, added token reducer

--- packages/exedao-app/src/store/exedao-middleware.js
+++ packages/exedao-app/src/store/exedao-middleware.js
@@ -1,7 +1,14 @@
-import { exeDAO } from 'exedao-js';
+import { ExeDAO } from 'exedao-js';
 import Web3 from 'web3';
 import { WEB3_SET } from './reducers/web3';
-import { EXEDAO_ADD_PROPOSAL, EXEDAO_SET_PROPOSAL_DETAILS, EXEDAO_ADD_VOTES, EXEDAO_SET } from './reducers/exedao';
+import {
+  EXEDAO_ADD_PROPOSAL,
+  EXEDAO_SET_PROPOSAL_DETAILS,
+  EXEDAO_ADD_TOKENS,
+  EXEDAO_ADD_VOTES,
+  EXEDAO_SET
+} from './reducers/exedao';
+import { dispatch } from 'rxjs/internal/observable/pairs';
 
 /* 
 function execute(action, next, dispatch) {
@@ -22,11 +29,13 @@ export default function createExedaoMiddleware() {
     await exedao.init()
     dispatch({type: EXEDAO_SET, exedao})
     
-    exedao.addProposalListener(
+    exedao.addListener(
+      'ProposalSubmission',
       async ({proposalHash}) =>
         dispatch({type: EXEDAO_ADD_PROPOSAL, proposal: await exedao.getProposal(proposalHash)}))
 
     exedao.addVoteListener(
+      'ProposalVote',
       ({proposalHash, votesCast}) =>
         dispatch({type: EXEDAO_ADD_VOTES, proposal: {proposalHash, votesCast}}))
   }
@@ -35,12 +44,24 @@ export default function createExedaoMiddleware() {
     if (exedao.ownedShares && !exedao.api.token) await exedao.api.login()
   }
 
+  async function addTokens(exedao) {
+    const tokens = await exedao.getTokens();
+    dispatch({
+      type: EXEDAO_ADD_TOKENS,
+      tokens: tokens.map(
+        ({tokenAddress, value}) =>
+          ({tokenAddress, balance: value}))
+    })
+  }
+
   return ({ dispatch }) => {
     return next => (action) => {
       const {web3, accounts, loggedIn, type} = action;
       if (type == WEB3_SET) {
-        const exedao = new exeDAO(web3, accounts[0], exeDAOAddress);
-        setExedao(exedao, dispatch).then(() => loggedIn && apiLogin(exedao));
+        const exedao = new ExeDAO(web3, accounts[0], exeDAOAddress);
+        setExedao(exedao, dispatch)
+          .then(() => loggedIn && apiLogin(exedao))
+          .then(() => addTokens(exedao));
       }
       return next(action);
     };
--- packages/exedao-app/src/store/reducers/exedao.js
+++ packages/exedao-app/src/store/reducers/exedao.js
@@ -3,21 +3,31 @@ export const EXEDAO_ADD_PROPOSAL = 'EXEDAO/ADD_PROPOSAL';
 export const EXEDAO_ADD_PROPOSALS = 'EXEDAO/ADD_PROPOSALS';
 export const EXEDAO_SET_PROPOSAL_DETAILS = 'EXEDAO/SET_PROPOSAL_DETAILS';
 
+export const EXEDAO_ADD_TOKEN = 'EXEDAO/ADD_TOKEN';
+export const EXEDAO_ADD_TOKENS = 'EXEDAO/ADD_TOKENS';
+export const EXEDAO_UPDATE_TOKEN = 'EXEDAO/ADD_TOKENS';
+
 export const EXEDAO_ADD_VOTES = 'EXEDAO/ADD_VOTES';
 
 const initialState = {
   exedao: null,
   proposals: [],
   proposalsMeta: [],
-  voteRequested: null
+  voteRequested: null,
+  tokens: []
 }
 
+const defaultTransform = (oldItem, newItem) => ({ ...oldItem, ...newItem })
+
+const updateItem = (arr, item, matchProperty, transform = defaultTransform) => arr.map((_item) => {
+    if (_item[matchProperty] == item[matchProperty]) return transform(_item, item)
+    return _proposal
+})
+
 export default (state = initialState, action) => {
-    const {type, proposals, proposal, exedao} = action
-    let _proposals
+    const {type, proposals, proposal, exedao, token, tokens} = action
     switch (type) {
         case EXEDAO_SET:
-            console.log('set exedao')
             return { ...state, exedao, loading: false }
 
         case EXEDAO_ADD_PROPOSAL:
@@ -33,27 +43,38 @@ export default (state = initialState, action) => {
             }
 
         case EXEDAO_SET_PROPOSAL_DETAILS:
-            // get current proposals in state that do not match the new proposal
-            _proposals = state.proposals.map((_proposal) => {
-                if (_proposal.proposalHash == proposal.proposalHash) return {..._proposal, ...proposal}
-                return _proposal
-            })
             return {
                 ...state,
-                proposals: _proposals
+                proposals: updateItem(state.proposals, _proposal, 'proposalHash')
             }
 
         case EXEDAO_ADD_VOTES:
-            const {proposalHash, votes} = proposal;
-            _proposals = state.proposals.map((_proposal) => {
-                if (proposalHash == _proposal.proposalHash) return {..._proposal, votes: _proposal.votes + votes}
-                return _proposal
-            });
             return {
                 ...state,
-                proposals: _proposals
+                proposals: updateItem(
+                    state.proposals, _proposal, 'proposalHash',
+                    (_proposal, {votes}) => ({ ..._proposal, votes: _proposal.votes + votes})
+                )
             }
-            
+        
+        case EXEDAO_ADD_TOKEN:
+            return {
+                ...state,
+                tokens: [...state.tokens, token]
+            }
+
+        case EXEDAO_ADD_TOKENS:
+            return {
+                ...state,
+                tokens: [...state.tokens, ...tokens]
+            }
+        
+        case EXEDAO_UPDATE_TOKEN:
+            return {
+                ...state,
+                tokens: updateItem(state.tokens, token, 'tokenAddress')
+            }
+
         default:
             return state
     }
