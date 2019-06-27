7072556002028e06116da8a9fb530a89049e410d
d1ll0n <dillonkellar@protonmail.com>
2019-06-27 17:37:52 -0500

    getTokenInfo in default export

--- packages/exedao-js/src/index.js
+++ packages/exedao-js/src/index.js
@@ -2,10 +2,12 @@ const ExeDAO = require('./ExeDAO');
 const deploy = require('./deploy');
 const abi = require('../build/abi');
 const bytecode = require('../build/bytecode');
+const getTokenInfo = require('./get-token-info')
 
 module.exports = {
   ExeDAO,
   deploy,
   abi,
-  bytecode
+  bytecode,
+  getTokenInfo
 };
