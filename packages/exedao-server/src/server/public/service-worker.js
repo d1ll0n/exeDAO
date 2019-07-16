a1ce6edfe8336f78d6fc1b0bd6721d1259041215
d1ll0n <dillonkellar@protonmail.com>
2019-07-15 20:11:03 -0500

    fixed web3 providers (this time I mean it)

--- packages/exedao-server/src/server/public/service-worker.js
+++ packages/exedao-server/src/server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78cdfab036f58ccd59afce00b92953a1.js"
+  "/precache-manifest.31394c00001302f7dd34f9b5fd262c9a.js"
 );
 
 workbox.clientsClaim();
