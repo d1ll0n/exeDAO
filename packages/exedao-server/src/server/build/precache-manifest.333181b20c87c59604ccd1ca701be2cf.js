230586948c921aea6ae77ef076ac673f01764350
d1ll0n <dillonkellar@protonmail.com>
2019-07-20 13:47:44 -0500

    update vote progress bar

---
+++ packages/exedao-server/src/server/build/precache-manifest.333181b20c87c59604ccd1ca701be2cf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "7274431c8fd05fe530dbe3bff660d47f",
+    "url": "/static/media/exedao-logo.7274431c.svg"
+  },
+  {
+    "revision": "a0e54a2957258a2dc7be",
+    "url": "/static/js/main.f5c9c721.chunk.js"
+  },
+  {
+    "revision": "dc3e3ae82d0dadbfaef310093e42c519",
+    "url": "/static/media/etherscan-logo.dc3e3ae8.png"
+  },
+  {
+    "revision": "5ff232157d19b921f7dc016519e3c577",
+    "url": "/static/media/discord-logo.5ff23215.png"
+  },
+  {
+    "revision": "be24b2b90b3df0c76fb38aaf0eab47e1",
+    "url": "/static/media/exedao-logo.be24b2b9.png"
+  },
+  {
+    "revision": "c5d5046b260f9c216b3141c6cd68bba7",
+    "url": "/static/media/torus.c5d5046b.svg"
+  },
+  {
+    "revision": "92a73d44a08b455c207ddd975ee713bc",
+    "url": "/static/media/metamask.92a73d44.png"
+  },
+  {
+    "revision": "9743252053ac48ba33cb",
+    "url": "/static/js/runtime~main.f9fddc3b.js"
+  },
+  {
+    "revision": "cc2b71ea7b93795c0c29a286cc9c94ef",
+    "url": "/static/media/fortmatic.cc2b71ea.png"
+  },
+  {
+    "revision": "2a77130f42d16b6963cb",
+    "url": "/static/js/5.c23554f5.chunk.js"
+  },
+  {
+    "revision": "a5997502cc6c700eaaf4",
+    "url": "/static/js/4.2605b277.chunk.js"
+  },
+  {
+    "revision": "89fa039ab05c530bc4a1",
+    "url": "/static/js/3.9fee4432.chunk.js"
+  },
+  {
+    "revision": "005d4c21455656a14a7a",
+    "url": "/static/js/2.831756cf.chunk.js"
+  },
+  {
+    "revision": "005d4c21455656a14a7a",
+    "url": "/static/css/2.db83da85.chunk.css"
+  },
+  {
+    "revision": "cee1b9be5628173356576f31722e053f",
+    "url": "/index.html"
+  }
+];
--- packages/exedao-server/src/server/public/static/js/4.2605b277.chunk.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{1621:function(n,o){},1629:function(n,o){},1631:function(n,o){}}]);
-//# sourceMappingURL=4.2605b277.chunk.js.map
